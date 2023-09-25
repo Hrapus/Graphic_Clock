a4a2fa9de0742329b29286a69c18c54facd887d4
Hrapus <hrapus@gmail.com>
2023-09-25 16:01:59 -0400

    Modify landscape XML

--- app/src/main/java/com/example/graphicclock/MainActivity.kt
+++ app/src/main/java/com/example/graphicclock/MainActivity.kt
@@ -72,18 +72,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun getImageResFromDigit(digit: Int?): Int {
         return when (digit) {
-            0 -> R.drawable.zero
-            1 -> R.drawable.one
-            2 -> R.drawable.two
-            3 -> R.drawable.three
-            4 -> R.drawable.four
-            5 -> R.drawable.five
-            6 -> R.drawable.six
-            7 -> R.drawable.seven
-            8 -> R.drawable.eight
-            9 -> R.drawable.nine
+            0 -> R.drawable.zero0
+            1 -> R.drawable.one1
+            2 -> R.drawable.two2
+            3 -> R.drawable.three3
+            4 -> R.drawable.four4
+            5 -> R.drawable.five5
+            6 -> R.drawable.six6
+            7 -> R.drawable.seven7
+            8 -> R.drawable.eight8
+            9 -> R.drawable.nine9
             else -> {
-                R.drawable.zero
+                R.drawable.zero0
             }
         }
     }
